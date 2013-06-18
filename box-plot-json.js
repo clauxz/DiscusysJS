4273586951bde0b13fd851d087af6866a9c4c2aa
Vivek Viswanathan <vivek1305@gmail.com>
2013-06-18 11:01:38 +0530

    committing the initial files

---
+++ box-plot-json.js
@@ -0,0 +1,46 @@
+var jsonData = {
+			tabularData: [
+				["","Mean AN","SD AN","Mean DI","SD DI","p"],
+				["Usability","25.3","4.5","30.2","5.5",0.2],
+				["Learnability","20.2","3.5","19.2","3.1",0.1],
+				["SUS","27.3","3.3","27.2","3.5",0.2]
+			],
+			data: [
+						{
+							category : 'Usability Analog',
+							categoryGroup : 'Usability',
+							data : [20, 27, 35, 40, 55],
+							dataGroup : 'Analog'
+						},
+						{
+							category : 'Usability Digital',
+							categoryGroup : 'Usability',
+							data : [27, 35, 40, 48, 58],
+							dataGroup : 'Digital'
+						},
+						{
+							category : 'Learnability Analog',
+							categoryGroup : 'Learnability',
+							data : [30, 35, 40, 55, 63],
+							dataGroup : 'Analog'
+						},
+						{
+							category : 'Learnability Digital',
+							categoryGroup : 'Learnability',
+							data : [2, 5, 8, 12, 17],
+							dataGroup : 'Digital'
+						},
+						{
+							category : 'SUS Analog',
+							categoryGroup : 'SUS',
+							data : [26, 35, 40, 45, 67],
+							dataGroup : 'Analog'
+						},
+						{
+							category : 'SUS Digital',
+							categoryGroup : 'SUS',
+							data : [10, 15, 20, 45, 57],
+							dataGroup : 'Digital'
+						}
+					]
+		}
