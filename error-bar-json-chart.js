c9a695e53b505aa1a4f241298e7dd2c7622bb427
Vivek Viswanathan <vivek1305@gmail.com>
2013-06-28 23:29:06 +0530

    Merged the latest code base and committing it. Added feature: Error-bar, box-plot and simple-bar plot

---
+++ error-bar-json-chart.js
@@ -0,0 +1,234 @@
+var jsonChartData = {
+    data: [
+        {
+            "Measurement": "Attach Sources",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Attach Sources AN.png"
+        },
+        {
+            "Measurement": "Attach Sources",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Attach Sources DI.png"
+        },
+        {
+            "Measurement": "Clarity",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Clarity AN.png"
+        },
+        {
+            "Measurement": "Clarity",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Clarity DI.png"
+        },
+        {
+            "Measurement": "Collaborative Affinity Diagram",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Collaborative Affinity Diagram AN.png"
+        },
+        {
+            "Measurement": "Collaborative Affinity Diagram",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Collaborative Affinity Diagram DI.png"
+        },
+        {
+            "Measurement": "Collaborative",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Collaborative AN.png"
+        },
+        {
+            "Measurement": "Collaborative",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Collaborative DI.png"
+        },
+        {
+            "Measurement": "Confidence of Points",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Confidence of Points AN.png"
+        },
+        {
+            "Measurement": "Confidence of Points",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Confidence of Points DI.png"
+        },
+        {
+            "Measurement": "Create Points",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Create Points AN.png"
+        },
+        {
+            "Measurement": "Create Points",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Create Points DI.png"
+        },
+        {
+            "Measurement": "Depth",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Depth AN.png"
+        },
+        {
+            "Measurement": "Depth",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Depth DI.png"
+        },
+        {
+            "Measurement": "Edit Points",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Edit Points AN.png"
+        },
+        {
+            "Measurement": "Edit Points",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Edit Points DI.png"
+        },
+        {
+            "Measurement": "Efficient",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Efficient AN.png"
+        },
+        {
+            "Measurement": "Efficient",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Efficient DI.png"
+        },
+        {
+            "Measurement": "Explain Points",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Explain Points AN.png"
+        },
+        {
+            "Measurement": "Explain Points",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Explain Points DI.png"
+        },
+        {
+            "Measurement": "Flow",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Flow AN.png"
+        },
+        {
+            "Measurement": "Flow",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Flow DI.png"
+        },
+        {
+            "Measurement": "Organizing Points",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Organizing Points AN.png"
+        },
+        {
+            "Measurement": "Organizing Points",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Organizing Points DI.png"
+        },
+        {
+            "Measurement": "Pleasant",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Pleasant AN.png"
+        },
+        {
+            "Measurement": "Pleasant",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Pleasant DI.png"
+        },
+        {
+            "Measurement": "Productivity",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Productivity AN.png"
+        },
+        {
+            "Measurement": "Productivity",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Productivity DI.png"
+        },
+        {
+            "Measurement": "Read Others Feedback",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Read Others Feedback AN.png"
+        },
+        {
+            "Measurement": "Read Others Feedback",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Read Others Feedback DI.png"
+        },
+        {
+            "Measurement": "Satisfaction of Result",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Satisfaction of Result AN.png"
+        },
+        {
+            "Measurement": "Satisfaction of Result",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Satisfaction of Result DI.png"
+        },
+        {
+            "Measurement": "Save to Report",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Save to Report AN.png"
+        },
+        {
+            "Measurement": "Save to Report",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Save to Report DI.png"
+        },
+        {
+            "Measurement": "Sources to Others",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Sources to Others AN.png"
+        },
+        {
+            "Measurement": "Sources to Others",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Sources to Others DI.png"
+        },
+        {
+            "Measurement": "Understand Others Points",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Understand Others Points AN.png"
+        },
+        {
+            "Measurement": "Understand Others Points",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Understand Others Points DI.png"
+        },
+        {
+            "Measurement": "Understand Report and Continue",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Understand Report and Continue AN.png"
+        },
+        {
+            "Measurement": "Understand Report and Continue",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Understand Report and Continue DI.png"
+        },
+        {
+            "Measurement": "Usability",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Usability AN.png"
+        },
+        {
+            "Measurement": "Usability",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Usability DI.png"
+        },
+        {
+            "Measurement": "View Others Points",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ View Others Points AN.png"
+        },
+        {
+            "Measurement": "View Others Points",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ View Others Points DI.png"
+        },
+        {
+            "Measurement": "Write Feedback",
+            "Type Short": "AN",
+            "Chart": "data/charts/P Hist QQ Write Feedback AN.png"
+        },
+        {
+            "Measurement": "Write Feedback",
+            "Type Short": "DI",
+            "Chart": "data/charts/P Hist QQ Write Feedback DI.png"
+        }
+	]
+}
